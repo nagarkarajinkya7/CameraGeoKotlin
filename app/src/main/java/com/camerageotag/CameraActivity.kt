24de9c800622a90484524921355f754d6a76d922
ajinkya <ajinkya@escanav.com>
2018-12-26 17:49:47 +0530

    main_activity creation

--- app/src/main/java/com/camerageotag/CameraActivity.kt
+++ app/src/main/java/com/camerageotag/CameraActivity.kt
@@ -2,11 +2,27 @@ package com.camerageotag
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.TextureView
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_camera.*
 
 class CameraActivity : AppCompatActivity() {
+
+    private var button: Button? = null
+    private var textView: TextView? = null
+    private var textureView: TextureView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        button = findViewById(R.id.getpicture) as Button
+        textView = findViewById(R.id.textView) as TextView
+        textureView = findViewById(R.id.textureview) as TextureView
     }
 }
